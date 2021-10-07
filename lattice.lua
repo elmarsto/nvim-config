809211194199e78fada971d162616b7db5f01e7b
Elizabeth 'elmarsto' Marston <elmarsto@microsoft.com>
2021-10-06 18:22:59 -0700

    gitignore plugin dir

--- lattice.lua
+++ lattice.lua
@@ -1,4 +1,3 @@
--- you are not expected to understand this
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
