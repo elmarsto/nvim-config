54d0cd24f580ddd0a06f2f50c40c3c107789c1ef
⁓ 𝓮𝓵𝓶𝓪𝓻𝓼𝓽𝓸 <elmarsto@microsoft.com>
2021-10-08 11:46:04 -0700

    updated example of a lattice_local.lua

--- lua/example-lattice_local.lua
+++ lua/example-lattice_local.lua
@@ -1,13 +1,13 @@
 return {
-    sumneko = { -- aboslute paths only, no expansion of ~, no expansion of $HOME
+    sumneko = { -- sumneko is picky. aboslute paths only, no expansion of ~, no expansion of $HOME
       root = "FIXME for whatever machine you're on";
       bin = "lua-language-server",
       main = "../main.lua"
     },
-    jsonls = { -- aboslute paths only, no expansion of ~, no expansion of $HOME
+    jsonls = {
       bin = "vscode-json-language-server",
     },
     sqlite = {
-      lib = "libsqlite3.so",
+      lib = "libsqlite3.so", -- under windows, an abs path to an sqlite .dll works here (grab from sqlite repo)
     }
 }
