8b5ee9f4a0707e938e031c5bc7ae4e43e0449836
⁓ 𝓮𝓵𝓶𝓪𝓻𝓼𝓽𝓸 <elmarsto@microsoft.com>
2021-10-12 20:53:56 -0700

    log-level autosession

--- lattice.lua
+++ lattice.lua
@@ -461,7 +461,7 @@ require("packer").startup {
       config = function()
         require("auto-session").setup(
           {
-            log_level = "info",
+            log_level = "error",
             auto_session_enabled = true,
             auto_save_enabled = true,
             auto_restore_enabled = true
