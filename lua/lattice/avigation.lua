d5f4982bb0f12ddec2944c3719de3b101f6020aa
lattice <elmarsto@users.noreply.github.com>
2022-12-28 01:05:29 -0800

    commit to flit/leap instead of hop

--- lua/lattice/avigation.lua
+++ lua/lattice/avigation.lua
@@ -39,55 +39,15 @@ function avigation.setup(use)
   }
   use {
     "ggandor/leap.nvim",
+    requires = { "ggandor/flit.nvim" },
     config = function()
       require('leap').add_default_mappings()
+      require('flit').setup()
     end
   }
   use "kana/vim-textobj-user"
   use "kana/vim-textobj-line"
   use "mbbill/undotree"
-  use {
-    "phaazon/hop.nvim",
-    branch = "v2",
-    as = "hop",
-    config = function()
-      local hop = require("hop")
-      hop.setup { keys = "etovxqpdygfblzhckisuran" }
-      local directions = require("hop.hint").HintDirection
-      vim.keymap.set(
-        "",
-        "f",
-        function()
-          hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-        end,
-        { remap = true }
-      )
-      vim.keymap.set(
-        "",
-        "F",
-        function()
-          hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-        end,
-        { remap = true }
-      )
-      vim.keymap.set(
-        "",
-        "t",
-        function()
-          hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-        end,
-        { remap = true }
-      )
-      vim.keymap.set(
-        "",
-        "T",
-        function()
-          hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-        end,
-        { remap = true }
-      )
-    end
-  }
   use {
     "stevearc/aerial.nvim",
     config = function()
