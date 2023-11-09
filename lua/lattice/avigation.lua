061415ab6bc06364a9a3750e6755cb1d45cbb3e4
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2023-11-08 18:40:09 -0800

    prog

--- lua/lattice/avigation.lua
+++ lua/lattice/avigation.lua
@@ -2,16 +2,16 @@ local avigation = {}
 function avigation.setup(use)
   use {
     "ggandor/leap.nvim",
-    requires = {
-      "ggandor/leap-spooky.nvim",
-      "ggandor/flit.nvim",
-    },
+    -- requires = {
+    --   "ggandor/leap-spooky.nvim",
+    --   "ggandor/flit.nvim",
+    -- },
     after = "vim-repeat",
     config = function()
       -- TODO: fix keybindings not to interfere with surround
       require('leap').add_default_mappings()
-      require('leap-spooky').setup()
-      require('flit').setup()
+      -- require('leap-spooky').setup()
+      -- require('flit').setup()
     end
   }
   use { "lukas-reineke/indent-blankline.nvim",
@@ -20,19 +20,19 @@ function avigation.setup(use)
     end
   }
   use "kiyoon/treesitter-indent-object.nvim"
-  use "tommcdo/vim-ninja-feet"
+  -- use "tommcdo/vim-ninja-feet"
   use "mbbill/undotree"
-  use { "nacro90/numb.nvim",
-    config = function()
-      require('numb').setup {
-        show_numbers = true,         -- Enable 'number' for the window while peeking
-        show_cursorline = true,      -- Enable 'cursorline' for the window while peeking
-        hide_relativenumbers = true, -- Enable turning off 'relativenumber' for the window while peeking
-        number_only = false,         -- Peek only when the command is only a number instead of when it starts with a number
-        centered_peeking = true,     -- Peeked line will be centered relative to window
-      }
-    end
-  }
+  -- use { "nacro90/numb.nvim",
+  --   config = function()
+  --     require('numb').setup {
+  --       show_numbers = true,         -- Enable 'number' for the window while peeking
+  --       show_cursorline = true,      -- Enable 'cursorline' for the window while peeking
+  --       hide_relativenumbers = true, -- Enable turning off 'relativenumber' for the window while peeking
+  --       number_only = false,         -- Peek only when the command is only a number instead of when it starts with a number
+  --       centered_peeking = true,     -- Peeked line will be centered relative to window
+  --     }
+  --   end
+  -- }
   use { "stevearc/oil.nvim",
     config = function()
       require "oil".setup({
